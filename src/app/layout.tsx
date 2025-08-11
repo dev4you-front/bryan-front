bc9842fae7eec2f39efb071447852d29e64b34bc
dev4you-front <contact@dev4you.fr>
2025-08-11 11:40:52 +0200

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ function Footer(): JSX.Element {
               <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
               </svg>
-              contact@example.com
+              bryanlittre.physio@gmail.com
             </p>
           </div>
           <div>
