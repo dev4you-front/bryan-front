c5586bc09f8d9b4d5a94d458af4e525802415f26
dev4you-front <contact@dev4you.fr>
2025-08-11 11:15:18 +0200

    Restaurer l'effet d'onde et supprimer la ligne

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>): JSX
         <main className="pb-12 bg-[#E0E0E0]">
           {children}
         </main>
-        <div className="bg-brandgray">
+        <div className="bg-[#E0E0E0] mb-[-1px]">
           <svg viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg" className="w-full block">
             <defs>
               <linearGradient id="sw-gradient-1" x1="0" x2="0" y1="1" y2="0">
