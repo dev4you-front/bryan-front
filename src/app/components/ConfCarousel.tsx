fc55793db705032414cffbd81040a88cad2bd36d
dev4you-front <contact@dev4you.fr>
2025-08-11 10:56:44 +0200

    Déplacer la pagination du carrousel sous la vidéo

--- src/app/components/ConfCarousel.tsx
+++ src/app/components/ConfCarousel.tsx
@@ -14,11 +14,11 @@ export default function ConfCarousel({ items }: ConfCarouselProps): JSX.Element 
   const goNext = (): void => setIndex((i: number) => (i + 1) % items.length);
 
   return (
-    <div className="relative flex justify-center items-center w-full my-12">
+    <div className="relative w-full my-12">
       {/* Prev */}
       <button
         aria-label="Précédent"
-        className="swiper-button-prev custom-swiper-arrow absolute left-0 md:-left-12 top-1/2 -translate-y-1/2 z-20"
+        className="swiper-button-prev custom-swiper-arrow absolute left-0 md:-left-12 top-1/3 -translate-y-1/2 z-20"
         onClick={goPrev}
       />
 
@@ -34,29 +34,29 @@ export default function ConfCarousel({ items }: ConfCarouselProps): JSX.Element 
             />
           </div>
           <p className="text-gray-700 text-center mt-6">{items[index]?.title}</p>
+          
+          {/* Pagination sous la vidéo */}
+          <div className="swiper-pagination custom-swiper-pagination mt-6">
+            {items.map((_, i) => (
+              <button
+                key={i}
+                aria-label={`Aller à la slide ${i + 1}`}
+                className={
+                  "swiper-pagination-bullet" + (i === index ? " swiper-pagination-bullet-active" : "")
+                }
+                onClick={(): void => setIndex(i)}
+              />
+            ))}
+          </div>
         </div>
       </div>
 
       {/* Next */}
       <button
         aria-label="Suivant"
-        className="swiper-button-next custom-swiper-arrow absolute right-0 md:-right-12 top-1/2 -translate-y-1/2 z-20"
+        className="swiper-button-next custom-swiper-arrow absolute right-0 md:-right-12 top-1/3 -translate-y-1/2 z-20"
         onClick={goNext}
       />
-
-      {/* Bullets (flow layout to push content down like Twig) */}
-      <div className="swiper-pagination custom-swiper-pagination mt-8">
-        {items.map((_, i) => (
-          <button
-            key={i}
-            aria-label={`Aller à la slide ${i + 1}`}
-            className={
-              "swiper-pagination-bullet" + (i === index ? " swiper-pagination-bullet-active" : "")
-            }
-            onClick={(): void => setIndex(i)}
-          />
-        ))}
-      </div>
     </div>
   );
 }
