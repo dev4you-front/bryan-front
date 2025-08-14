bd0fc70d6cd67abf5056c1560636810bf46581d4
dev4you-front <contact@dev4you.fr>
2025-08-15 00:00:06 +0200

    Updated blogArticles.ts

--- src/data/blogArticles.ts
+++ src/data/blogArticles.ts
@@ -2,7 +2,7 @@ import { Article } from "@/types";
 
 export const blogArticles: Article[] = [
   {
-    title: "En fait, comment ça marche un étirement ?",
+    title: "Biomécanique des étirements – Partie II",
     image: "https://www.kinefact.com/wp-content/uploads/2019/10/cat-814952_1920.jpg",
     excerpt: "Dans l'article précédent, Bryan Littre nous a présenté les différentes théories pouvant expliquer les gains obtenus par les diverses techniques d'étirement, en s'appuyant notamment sur les travaux de Moltubakk. Dans l'article du jour, on va regarder comment la littérature peut nous aider à trancher en faveur de l'une ou de l'autre.",
     date: "2019-10-04",
