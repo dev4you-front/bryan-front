6ecaf83c7bba9713787890a00691b1e5b18111a9
dev4you-front <contact@dev4you.fr>
2025-08-14 20:14:47 +0200

    Configuration des domaines d'images pour le blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.kinefact.com',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
+  },
   async headers() {
     return [
       {
