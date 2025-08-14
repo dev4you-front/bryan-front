1806e8f67042fae1802d19a49620a62c154d584e
dev4you-front <contact@dev4you.fr>
2025-08-14 19:24:22 +0200

    Créer le troisième article de blog en Markdown

--- src/data/blogArticles.ts
+++ src/data/blogArticles.ts
@@ -173,6 +173,144 @@ export const blogArticles: Article[] = [
     `
   },
   {
+    title: "Syndrome du canal carpien, pourquoi sommes-nous négligés du parcours de soin ?",
+    image: "https://www.kinefact.com/wp-content/uploads/2021/01/grossesse-4-remedes-naturels-contre-le-syndrome-du-canal-carpien-quand-on-est-enceinte.jpeg",
+    excerpt: "Le syndrome du canal carpien est une atteinte autour de laquelle beaucoup de croyances existent, au même titre que la plupart des névralgies. Cette série d'articles fait une mise à jour détaillée autour du syndrome du canal carpien (SCC) basée sur la littérature scientifique.",
+    date: "2021-02-13",
+    author: "Bryan Littré",
+    link: "/blog/syndrome-canal-carpien-bases-comprehension",
+    content: `
+# Syndrome du canal carpien, pourquoi sommes-nous négligés du parcours de soin ?
+
+Par Bryan Littré, remerciements et aide à la relecture : **Marguerite Dontenwille, Robin Vervaeke, Marie Akrich, Antoine Massuleau, Benjamin Heng**
+
+Avez-vous remarqué ce titre aguicheur ? Malheureusement il est véridique. Le syndrome du canal carpien est une atteinte autour de laquelle beaucoup de croyances existent, au même titre que la plupart des névralgies. J'ai pu m'en rendre compte durant l'élaboration de mon mémoire de fin d'étude de kiné qui a mené à cette publication :
+
+## [Neurodynamique et neuropathie compressive du membre supérieur : revue systématique](https://www.sciencedirect.com/science/article/abs/pii/S1779012317307003)
+
+Le but de cette série d'articles est de faire une mise à jour détaillée autour du syndrome du canal carpien (SCC). Nous devons donc nous baser sur des connaissances qui reflètent le mieux notre savoir partageable sur ce sujet, voilà pourquoi tout cet article sera soutenu par la littérature scientifique. Pour prendre des bonnes décisions il faut déjà commencer par avoir de bonnes données, *« si on a des données merdiques on fait de la merde »*.
+
+Je tiens juste à préciser que les problèmes nerveux font partie d'un de mes domaines d'expertise et dispensant des formations pour professionnels de santé dans la prise en charge des neuropathies, névralgies, traitement neurodynamique et syndrome canalaires. Ayant donc des liens d'intérêts, toute donnée présentée sera soutenue par des données de la littérature scientifique, ce qui me semble le minimum même si je n'avais aucun lien d'intérêt, car nous allons discuter de santé pas de cuisine et dans la santé, la vie ou la qualité de vie des gens est en jeu.
+
+<video width="480" height="480" loop autoplay muted>
+  <source src="https://www.kinefact.com/wp-content/uploads/2021/01/giphy-31.mp4" type="video/mp4">
+</video>
+
+**Cette petite première partie est un préambule afin d'avoir une base épistémique afin de mieux maitriser les deux autres billets qui vont suivre. La seconde partie traitement des spécificité du canal carpien et la dernière du traitement le plus approprié décris en détails.**
+
+En 1997, un groupe de travail français a tenté de définir le syndrome du canal carpien (SCC)
+
+« *Pour le groupe de travail, le syndrome du canal carpien est l'ensemble des symptômes ressentis par le patient et des signes constatés par le médecin. Plusieurs facteurs, imparfaitement définis, interviendraient dans les contraintes subies par le nerf médian dans la traversée du canal carpien (compressions, tractions ou autres). À côté de la compression chronique du nerf médian au niveau du canal carpien, il existe également des formes rares : compressions isolées de la branche motrice (transligamentaires) et compressions aiguës. Lorsque aucune cause n'est retrouvée, le syndrome du canal carpien est dit « idiopathique » (cas le plus fréquent). – Les critères cliniques ne sont pas pathognomoniques mais sont souvent évocateurs du syndrome du canal carpien. Il existe des critères électromyographiques pathognomoniques, mais inconstamment présents. L'aspect macroscopique per-opératoire est souvent normal, sauf dans les formes sévères. Il n'y a pas de signe anatomo-pathologique pathognomonique du syndrome du canal carpien.* »
+
+*Agence Nationale d'Accréditation et d'Evaluation en Santé. (1997). Stratégie des examens paracliniques et des indications thérapeutiques dans le syndrome du canal carpien. Recommandations et références médicales, 201-13.*
+
+Cette définition est à mon sens l'une des plus juste et adaptée au vu des données de l'époque. Ce groupe semblait tout à fait comprendre ce qu'était un syndrome du canal carpien. En effet, même si nous pourrions croire que c'est simple de créer cette définition, il y a des subtilités extrêmement importantes pouvant amener à des erreurs de compréhension de ce que sont les neuropathies et les névralgies, et ces erreurs ont été faites durant les dernières recommandations HAS de 2012.
+
+<video width="640" height="490" loop autoplay muted>
+  <source src="https://www.kinefact.com/wp-content/uploads/2021/01/sEP-YkAuT6W7l5SL.mp4" type="video/mp4">
+</video>
+
+## Ce n'est qu'un syndrome
+
+Voici ma définition du syndrome du canal carpien : Les SCC correspondent à une plainte se présentant au moins par des douleurs neuropathiques ou des névralgies et/ou neuropathies en regard du territoire du nerf médian, à partir du canal carpien, accompagnées plus ou moins d'acroparesthésies nocturnes. Cette description est à la fois la plus large et la plus précise.
+
+La simple notion de syndrome est problématique car fourre-tout. A la fois elle peut être utile pour étiqueter certains patients en les mettant dans des cases de par leur schéma clinique, mais justement cet étiquetage peut nous amener à perdre en précision et diminuer la qualité des soins.
+
+Explorons les notions de **maladie**, **syndrome** et **symptôme** à travers l'épistémologie afin de comprendre toute la subtilité du propos. Nous avons donc besoin de définitions.
+
+L'épistémologie est une branche de la philosophie qui a pour objet l'étude critique des postulats, conclusions et méthodes d'une science particulière, considérée du point de vue de son évolution afin d'en déterminer l'origine logique, la valeur et la portée scientifique et philosophique (cf. philosophie des sciences, empirisme logique).
+
+**Diagnostic :** l'origine étymologique du mot "*diagnostic*" nous vient du grec DIA – « séparément » et GIGNOSKEIN « savoir, percevoir, penser, juger »
+
+Un diagnostic, c'est donc l'art d'identifier une maladie d'après ses signes, ses symptômes. Cela peut être définie également comme une conclusion, généralement prospective, faisant suite à l'examen analytique d'une situation souvent jugée critique ou complexe.
+
+**Qu'est ce qu'un symptôme ?** Un symptôme est la manifestation spontanée d'un état ou d'une maladie permettant de la déceler, qui est perçue subjectivement par le sujet ou constatée objectivement par un observateur.
+
+Je vous propose un petit exemple :
+
+- des sueurs,
+- une pâleur,
+- une fringale,
+- une vision floue,
+- des tremblements,
+- une sensation de faiblesse,
+- des troubles de l'humeur (tristesse, agressivité ou euphorie).
+
+Sont les symptômes de l'hypoglycémie. Cependant, pour une même atteinte / maladie, les signes et symptômes peuvent totalement devenir différents selon la durée de l'atteinte et selon le niveau d'atteinte. Par exemple lorsque l'hypoglycémie perdure, une neuroglucopénie peut être observée.
+
+**La neuroglucopénie :** elle peut produire une variété d'effets et symptômes survenant pour un seuil glycémique inférieur à 0,50 gl·-1 : sensation de malaise avec asthénie importante, troubles de la concentration intellectuelle, sensation de dérobement des jambes, paresthésie des extrémités, céphalées, impressions vertigineuses, troubles psychiatriques, multiples et trompeurs (confusion aiguë, agitation, troubles de l'humeur et du comportement, état pseudo-ébrieux…), troubles neurologiques sévères (crises convulsives généralisées ou localisées), troubles moteurs déficitaires, troubles visuels à type de diplopie ou de vision trouble.
+
+L'hypoglycémie sévère durable (glycémie inférieure à 0,20 gl·-1 pendant plus de deux heures) peut induire une nécrose cellulaire responsable de séquelles.
+
+Certains médicaments produisent une hypoglycémie, c'est notamment le cas des médicaments anti-diabétiques, comme l'insuline et certains antidiabétiques oraux chez les sujets diabétiques. D'autres médicaments peuvent être mis en cause, comme l'aspirine (essentiellement chez le nourrisson et le petit enfant, à très forte dose), anti-inflammatoires non stéroïdiens, et quinidine.
+
+Les hypoglycémies ont plusieurs catégories de causes : soit un excès d'insuline ; soit un défaut de production de glucose. L'excès d'insuline peut être endogène, lié à un surdosage, le plus souvent tumoral, par une tumeur du pancréas, l'insulinome, une tumeur rare, moins d'un cas pour un million d'individus par an. L'hypoglycémie se rapproche de ce que l'on nomme un syndrome.
+
+[Hypoglycémie — Wikipédia](https://fr.wikipedia.org/wiki/Hypoglyc%C3%A9mie)
+
+J'espère que vous vous rendez bien compte que pour traiter une hypoglycémie par tumeur du pancréas, ou une parce que Jean-Michel JeNeDéjeunePas fait un petit malaise dans le RER B à 8h04, ne se règlent pas de la même manière ? Bah c'est pareil pour le « syndrome » du canal carpien.
+
+La subtilité la plus importante pour nous reste la distinction entre syndrome et maladie.
+
+### Syndrome
+
+MÉD., PATHOL. Ensemble de signes, de symptômes, de modifications morphologiques, fonctionnelles ou biochimiques de l'organisme, d'apparence parfois disparate mais formant une entité reconnaissable qui, sans présager obligatoirement des causes de ces manifestations, permettent d'orienter le diagnostic.
+
+*Source : https://www.cnrtl.fr/definition/syndrome*
+
+### Maladie
+
+[Chez l'homme] Altération de l'état de santé se manifestant par un ensemble de signes et de symptômes perceptibles directement ou non, correspondant à des troubles généraux ou localisés, fonctionnels ou lésionnels, dus à des causes internes ou externes et comportant une évolution.
+
+*Source : https://www.cnrtl.fr/definition/maladie*
+
+Pour partir sur de bonnes bases, il est nécessaire de connaître la signification première de ces deux termes, afin de mieux comprendre ce qui se cache derrière. La maladie est une altération ou un trouble d'un organisme vivant. Elle est forcément pathologique. Le syndrome, lui, est un ensemble de symptômes ou de signes cliniques, peut-être pathogène ou non, contrairement à la maladie qui l'est obligatoirement.
+
+Le patient peut présenter ces symptômes lorsqu'il a une maladie, ou bien lorsqu'il présente des écarts par rapport à la norme. Une maladie est un état pathologique, qui peut avoir plusieurs symptômes. Un syndrome est assez imprécis pour pouvoir intégrer plusieurs diagnostics plus ou moins précis. Et c'est là toute la différence entre les deux termes. Néanmoins, beaucoup de maladies portent le nom de syndrome, comme on peut le voir plus bas. Le syndrome peut aussi être le signe d'une maladie. D'autres fois, il peut être la conséquence.
+
+<img src="https://www.kinefact.com/wp-content/uploads/2021/01/1-e1611870736468-300x112.png" alt="Schéma syndrome méningé" width="370" height="138">
+
+Dans l'exemple ci-dessus, on voit que l'association céphalées + vomissements + raideur de la nuque + photophobie constitue un syndrome méningé traduisant l'irritation des méninges (plus précisément l'inflammation de l'espace sous-arachnoïdien). Le médecin pratique alors une ponction du liquide céphalo-rachidien qui va l'orienter vers la maladie en cause : hémorragie méningée si le liquide contient du sang, méningite purulente si le liquide est trouble, etc.
+
+Le diagnostic d'une maladie est donc bien différent du syndrome qui est rattaché à elle. Les étapes du raisonnement clinique ne sont pas les mêmes. N'oublions pas non plus qu'un diagnostic est évolutif et que selon les recherches actuelles, les diagnostics établis peuvent devenir obsolètes. Comme le montre l'évolution de la définition de « l'homosexualité », qui a été considérée comme une **MALADIE PSYCHIATRIQUE jusqu'en 1974 aux USA et 1992 en France**
+
+*Sources :*
+- *http://www.slate.fr/story/41351/homosexualite-maladie*
+- *BRIKI, Malick. Psychiatrie et homosexualité. Presses Univ. Franche-Comté, 2009.*
+
+<video width="480" height="266" loop autoplay muted>
+  <source src="https://www.kinefact.com/wp-content/uploads/2021/01/giphy-4.mp4" type="video/mp4">
+</video>
+
+Les diagnostics peuvent se préciser uniquement s'ils se détachent des autres atteintes au sein d'un syndrome. On parlera ici de diagnostic différentiel MAIS si un syndrome est émis ou mis en évidence, alors le niveau de vigilance devrait être augmenté afin de trier les différentes maladies ou les différentes causes pouvant amener à ce syndrome. La recherche de « causes » est utile uniquement pour des aspects prescriptifs, donc si un traitement adapté à la condition existe et se montre plus efficient ou pertinent. Voici deux exemples différents afin d'illustrer mon propos : le traitement semble identique si un patient est atteint d'un syndrome fémoro patellaire ou d'une tendinopathie patellaire, par contre il peut être différent si il présente un syndrome de traversé thoraco-brachiale suite à une radiculalgie ou suite un une tumeur de l'apex pulmonaire ou Pancoast Tobias.
+
+Le diagnostic différentiel est une méthode permettant de différencier une maladie parmi d'autres qui présentent des symptômes proches ou similaires. Ce processus vise à établir un diagnostic plus sûr via une approche méthodique qui prend en considération tant les éléments permettant d'exclure une maladie que ceux permettant de la confirmer. Le diagnostic différentiel peut aboutir à plusieurs hypothèses impossibles à départager à un instant donné, mais qui seront infirmées ou confirmées au cours du temps ou à l'aide d'examens complémentaires.
+
+*Source : http://www.chu-rouen.fr/page/diagnostic-differentiel*
+
+Le SYNDROME du canal carpien (SCC) est donc un label, s'exprimant cliniquement d'une manière +/- distincte mais pouvant intégrer une population hétérogène. Ce label a des impacts sociologiques, de reconnaissance, permet de pouvoir ajuster des traitements et d'obtenir un pronostic, des démarches administratives ou des thérapeutiques plus efficaces.
+
+Le syndrome du canal carpien n'est donc pas forcément une mononeuropathie compressive au niveau du poignet. Le groupe de patients ayant un SCC contient un nombre de patients ayant une mononeuropathie compressive au niveau du poignet, mais également des gens ayant des troubles vasculaires, des troubles neurologiques autres …
+
+Les définitions de l'incidence et de la prévalence vont nous être utiles pour la suite voici donc leur définition classiquement admise :
+
+- **L'incidence** mesure le nombre de nouveaux cas d'une maladie pendant une période de référence, souvent un an. De manière plus précise, l'incidence est la proportion des individus atteints par une maladie donnée par référence au nombre total de sujets présents dans la population étudiée, non malades au début de l'étude, et cela pendant une période donnée.
+
+- **La prévalence** est une mesure de l'état de santé d'une population à un instant donné, pour un problème sanitaire donné. Tout comme un sondage d'opinion, c'est une photographie instantanée, qui ne vaut que pour l'instant étudié. La prévalence d'une affection est calculée en rapportant à la population globale le nombre de malades présents dans cette population, qu'il s'agisse de nouveaux ou d'anciens cas. La prévalence est une proportion, en règle générale exprimée par un pourcentage. C'est un rapport du nombre de cas d'un trouble morbide à l'effectif total d'une population, sans distinction entre les cas nouveaux et les cas anciens, à un moment ou pendant une période donnée.
+
+*Sources :*
+- *https://fr.wikipedia.org/wiki/Incidence_(épidémiologie)*
+- *https://fr.wikipedia.org/wiki/Prévalence*
+- *https://www.vocabulaire-medical.fr/encyclopedie/226-frequence-incidence-occurrence-prevalence*
+
+<img src="https://www.kinefact.com/wp-content/uploads/2021/01/unnamed-1-300x202.jpg" alt="Graphique incidence prévalence" width="413" height="278">
+
+Voici la fin de la première partie, permettant de mettre un socle commun pour la suite. Les articles 2 puis 3 explorerons avec finesse les données acquises sur le SCC ainsi que son traitement optimal en kinésithérapie. Sommes-nous légitimes à prendre en charge cette atteinte ?
+
+POUR LA PARTIE 2 cliquez [**ICI**](https://www.kinefact.com/?p=2108&preview=true)
+`
+  },
+  {
     title: "En fait, comment ça marche un étirement ?",
     image: "https://www.kinefact.com/wp-content/uploads/2019/10/cat-814952_1920.jpg",
     excerpt: "Dans l'article précédent, Bryan Littre nous a présenté les différentes théories pouvant expliquer les gains obtenus par les diverses techniques d'étirement, en s'appuyant notamment sur les travaux de Moltubakk. Dans l'article du jour, on va regarder comment la littérature peut nous aider à trancher en faveur de l'une ou de l'autre. En conclusion, Bryan raconte comment la connaissance de ces données très théoriques a un réel impact sur sa pratique.",
