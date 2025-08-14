a16d185b4ba856af4f29c7455381aa8828337b31
dev4you-front <contact@dev4you.fr>
2025-08-14 23:27:41 +0200

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,6 @@ const nextConfig = {
         port: '',
         pathname: '/wp-content/uploads/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
   async headers() {
