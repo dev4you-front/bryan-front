796df95f2d89630457df6935958c4ed8e2325151
dev4you-front <contact@dev4you.fr>
2025-08-15 01:30:49 +0200

    Ajout de media-src à la CSP

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-src 'self' https://*.youtube.com https://*.youtube-nocookie.com https://physiomaps.com; default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://*.youtube.com https://*.youtube-nocookie.com https://*.googlevideo.com https://*.gstatic.com;"
+            value: "frame-src 'self' https://*.youtube.com https://*.youtube-nocookie.com https://physiomaps.com; default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self' https://*.youtube.com https://*.youtube-nocookie.com https://*.googlevideo.com https://*.gstatic.com; media-src 'self' https://www.kinefact.com;"
           }
         ]
       }
