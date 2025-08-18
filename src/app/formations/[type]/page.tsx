5aa571f4b5d1a4408b717e62ed8f42ad6d2ef9bf
dev4you-front <contact@dev4you.fr>
2025-08-18 18:47:58 +0200

    Updated page.tsx

--- src/app/formations/[type]/page.tsx
+++ src/app/formations/[type]/page.tsx
@@ -50,7 +50,7 @@ export default async function FormationTypePage({ params }: Props) {
         <div className="bg-white rounded-2xl shadow-lg p-8">
           <h2 className="text-center text-3xl md:text-4xl font-extrabold text-gray-900 mb-6 uppercase tracking-wide">
             Troubles vasculaires : 
-            Trier pour savoir quand traiter !
+            Trier pour savoir quand traiter ! 
           </h2>
           <ConfCarousel items={videos} />
         </div>
