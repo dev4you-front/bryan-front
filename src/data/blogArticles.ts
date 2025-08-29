f40722a4692b6b3a3ff4d8cce9da1908dda00add
dev4you-front <contact@dev4you.fr>
2025-08-29 19:45:40 +0200

    Updated blogArticles.ts

--- src/data/blogArticles.ts
+++ src/data/blogArticles.ts
@@ -62,7 +62,7 @@ export const blogArticles: Article[] = [
     category: "Sport",
   },
   {
-    title: "Les lésions aiguës des ischio-jambiers : comprendre et optimiser la prise en charge",
+    title: "Les lésions aiguës des ischio-jambiers (2/4) : comprendre et optimiser la prise en charge",
     image: "https://www.kinefact.com/wp-content/uploads/2020/07/og-image-07.jpg",
     excerpt: "Lors de la première partie, nous avons exploré quelques problématiques liées aux prescriptions médicales en France, le haut taux de récurrence, d'incidence et de prévalence. Maintenant passons à la suite et entrons dans le vif du sujet avec les subtilités de la prise en charge.",
     date: "2020-08-07",
